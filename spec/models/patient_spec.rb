15b05d085bce93c0dc3f2a9cd9c994415fb5f38f
Amy Spears <amspears007@gmail.com>
2023-04-24 11:43:38 -0600

    Add adult model method to pass model spec

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe Patient do
   let!(:katie) {Patient.create(name: "Katie L", age: 30)}
   let!(:brandon) {Patient.create(name: "Brandon J", age: 31)}
   let!(:amy) {Patient.create(name: "Amy S", age: 40)}
+  let!(:penny) {Patient.create(name: "Amy S", age: 8)}
+
 
   let!(:dr_quinn_amy) {DoctorPatient.create!(doctor_id: dr_quinn.id, patient_id: amy.id)}
   let!(:dr_quinn_katie) {DoctorPatient.create!(doctor_id: dr_quinn.id, patient_id: katie.id)}
@@ -16,8 +18,12 @@ RSpec.describe Patient do
 
   it {should have_many :doctor_patients}
   it {should have_many(:doctors).through(:doctor_patients)}
-end
 
-describe 'get names of all patients with age > 18' do
-  expect(Patient.adult).to eq([katie, brandon, amy])
+
+  describe 'get names of all patients with age > 18' do
+    it '.adult' do
+
+      expect(Patient.adult).to match_array([katie, brandon, amy])
+    end
+  end
 end
