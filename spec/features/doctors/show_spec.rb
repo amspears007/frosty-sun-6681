6f4ac119501b18c5d7d54381e606bd9d5aab4237
Amy Spears <amspears007@gmail.com>
2023-04-24 11:00:22 -0600

    Add controller and view for docotrs

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe 'Doctor Show Page', type: :feature do
   let!(:denver_health) {Hospital.create!(name: "DenverHealth")}
-  let!(:dr_quinn) {denver_health.doctors.create!(name: "Michaela Quinn", specialty: "rural health", university: "Boston")}
+  let!(:dr_quinn) {denver_health.doctors.create!(name: "Michaela Quinn", specialty: "Rural Health", university: "Boston")}
   let!(:dr_house) {denver_health.doctors.create!(name: "Gregory House", specialty: "ID", university: "Johns Hopkins")}
   let!(:dr_mccoy) {denver_health.doctors.create!(name: "Leonard Mccoy", specialty: "bones", university: "University")}
 
@@ -17,10 +17,11 @@ RSpec.describe 'Doctor Show Page', type: :feature do
   describe "US 1 when I visit /doctors/:id" do
   it " I see all of that doctor's information including:name, specialty university where they got their doctorate" do
     visit "/doctors/#{dr_quinn.id}"
+    save_and_open_page
 
-    within(".project_info") do
+    within(".doctor_info") do
       expect(page).to have_content("Name: Michaela Quinn")
-      expect(page).to have_content("Specialty: rural health")
+      expect(page).to have_content("Specialty: Rural Health")
       expect(page).to have_content("University: Boston")
       expect(page).to_not have_content("Name:Gregory House")
         end
